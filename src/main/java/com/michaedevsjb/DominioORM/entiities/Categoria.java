69d43877e11468fdb96148a7ddfd4d8c83990ece
Michael Gonçalves <michaeldev.sjb@gmail.com>
2024-12-24 07:47:05 -0300

    Categoria Criada

---
+++ src/main/java/com/michaedevsjb/DominioORM/entiities/Categoria.java
@@ -0,0 +1,56 @@
+package com.michaedevsjb.DominioORM.entiities;
+
+import java.util.Objects;
+
+public class Categoria {
+
+    private Integer id;
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(String descricao, Integer id) {
+        this.descricao = descricao;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categoria other = (Categoria) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+}
---
+++ src/test/java/com/michaedevsjb/DominioORM/DominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaedevsjb.DominioORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
